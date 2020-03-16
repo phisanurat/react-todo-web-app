b4c3a75623a95ffb2f61f1419e9b2f3dba02779e
DarkHeart <phisanurat.won@msu.ac.th>
2020-03-16 13:08:04 +0700

    refactor

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 // import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
-import React, {Component} from 'react';
-import './styles.css'
-import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
+import React, { Component } from "react";
+import "./styles.css";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Todo from "./todo";
 import List from "./List";
 import Home from "./Home";
 
 class App extends Component {
-
-    render() {
-        return (
-            <Router>
-                <Switch>
-                    <Route path="/" exact component={Todo}/>
-                    <Route path="/home" exact component={Home}/>
-                </Switch>
-            </Router>
-        );
-    }
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Todo} />
+          <Route path="/home" exact component={Home} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
-
--- src/Home.js
+++ src/Home.js
@@ -1,32 +1,42 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class Home extends Component {
-    render() {
-        return (
-            <div className={"pos-f-t"}>
-                {/*<div className={"container-fluid"}>*/}
-                <nav className="navbar navbar-expand-lg navbar-light bg-light navbar navbar-dark bg-dark">
-                    <a className="navbar-brand" href="#">M e m o</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse"
-                            data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup"
-                            aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div className="navbar-nav">
-                            <a className="nav-item nav-link active" href="#">Home
-                                {/*<span className="sr-only">(current)</span>*/}
-                            </a>
-                            <a className="nav-item nav-link" href="/">Todo</a>
-                            {/*<a className="nav-item nav-link disabled" href="#">Shared</a>*/}
-                        </div>
-                    </div>
-                </nav>
-                {/*</div>*/}
-
+  render() {
+    return (
+      <div className={"pos-f-t"}>
+        {/*<div className={"container-fluid"}>*/}
+        <nav className="navbar navbar-expand-lg navbar-light bg-light navbar navbar-dark bg-dark">
+          <a className="navbar-brand" href="#">
+            M e m o
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <a className="nav-item nav-link active" href="#">
+                Home
+                {/*<span className="sr-only">(current)</span>*/}
+              </a>
+              <a className="nav-item nav-link" href="/">
+                Todo
+              </a>
+              {/*<a className="nav-item nav-link disabled" href="#">Shared</a>*/}
             </div>
-        );
-    }
+          </div>
+        </nav>
+        {/*</div>*/}
+      </div>
+    );
+  }
 }
 
 export default Home;
